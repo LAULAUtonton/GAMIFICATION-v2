4a4a453186252c8603e667caa85b907ec5948863
LAULAUtonton <lestevezplasencia@gmail.com>
2026-04-06 23:50:52 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ const Home = () => {
       const response = await axios.get(`${API}/`);
       console.log(response.data.message);
     } catch (e) {
-      console.error(e, `errored out requesting / api`);
+      console.error(e, "errored out requesting / api");
     }
   };
 
@@ -29,7 +29,10 @@ const Home = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4" />
+          <img
+            src="https://avatars.githubusercontent.com/in/1201222?s=120&u=2686cf91179bbafbc7a71bfbc43004cf9ae1acea&v=4"
+            alt="Emergent logo"
+          />
         </a>
         <p className="mt-5">Building something incredible ~!</p>
       </header>
@@ -42,9 +45,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<Home />} />
-          </Route>
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
